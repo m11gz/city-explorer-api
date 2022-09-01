1674a9bc0d786c85cbd61b58f7a2825082b37d3a
Miguel Rodriguez <7migro11@gmail.com>
2022-08-31 23:46:35 -0700

    live weather

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios');
+// const axios = require('axios');
+const weatherHandler = require('./weather.js');
 
 require('dotenv').config();
 
@@ -17,31 +18,38 @@ app.get('/', (request, response) => {
   response.send('Hello there!');
 });
 
-app.get('/weather', async (request, response, next) => {
-  console.log(request.query.city_name);
-  try {
-    let lat = request.query.lat;
-    let lon = request.query.lon;
-    let urlWeather = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.REACT_APP_WEATHER_API_KEY}&lat=${lat}&lon=${lon}&units=I&days=3`;
-    let weatherObj = await axios.get(urlWeather);
-    let selectedCity = weatherObj.data.data.map((day) => new Forecast(day));
-    response.send(selectedCity);
-  } catch (err) {
-    next(err);
-  }
-});
+app.get('/weather', weatherHandler);
+
+// app.get('/weather', async (request, response, next) => {
+//   console.log(request.query.city_name);
+//   try {
+//     let lat = request.query.lat;
+//     let lon = request.query.lon;
+//     let urlWeather = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.REACT_APP_WEATHER_API_KEY}&lat=${lat}&lon=${lon}&units=I&days=3`;
+//     let weatherObj = await axios.get(urlWeather);
+//     let selectedCity = weatherObj.data.data.map((day) => new Forecast(day));
+//     response.send(selectedCity);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 app.get('*', (request, response) => {
   response.send('Oh, sorry. That is not found in our database.');
 });
 
-class Forecast {
-  constructor(day) {
-    this.description = day.weather.description;
-    this.date = day.datetime;
-    this.highTemp = day.max_temp;
-    this.lowTemp = day.low_temp;
-  }
-}
+// class Forecast {
+//   constructor(day) {
+//     this.description = day.weather.description;
+//     this.date = day.datetime;
+//     this.highTemp = day.max_temp;
+//     this.lowTemp = day.low_temp;
+//   }
+// }
+
+// app.use((err, req, res, next) => {
+//   console.log(err.message);
+//   res.status(500).send(err.message);
+// });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
